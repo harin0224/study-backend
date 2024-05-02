ac20dd65a8f460d861d254ca43867b6c78bb76fd
HyesonAnn <side111229@gmail.com>
2024-05-02 15:47:28 +0900

    lambda

---
+++ 02_java/m9_Lambda/src/com/multi/lambda/App.java
@@ -0,0 +1,14 @@
+package com.multi.lambda;
+
+import com.multi.lambda.a_basic.A_Lambda;
+import com.multi.lambda.a_basic.B_Functional;
+import com.multi.lambda.b_standard_function.A_Consumer;
+
+public class App {
+    public static void main(String[] args) {
+        // new A_Lambda().method1();
+        // new B_Functional().method3();
+        new A_Consumer().method1();
+
+    }
+}
---
+++ 02_java/m9_Lambda/src/com/multi/lambda/a_basic/functional/C_FuncInterface.java
@@ -0,0 +1,7 @@
+package com.multi.lambda.a_basic.functional;
+
+public interface C_FuncInterface {
+    // 메개변수는 있고 반환값도 있는 추상 메소드
+    public int method(int a, int b);
+
+}
