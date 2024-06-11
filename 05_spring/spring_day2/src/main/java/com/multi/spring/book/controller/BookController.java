93915fd8b26085d6c115388f0439570f3248f779
HyesonAnn <side111229@gmail.com>
2024-06-11 09:03:35 +0900

    spring chat

--- 05_spring/spring_day2/src/main/java/com/multi/spring/book/controller/BookController.java
+++ 05_spring/spring_day2/src/main/java/com/multi/spring/book/controller/BookController.java
@@ -1,29 +1,29 @@
-package com.multi.spring.book.controller;
-
-import java.util.List;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.multi.spring.book.model.dto.BookDTO;
-
-
-@Controller
-@RequestMapping("/book") 
-public class BookController {
-	
-	@GetMapping("/book")
-	public void foodForm() {
-		
-	}
-	
-	@GetMapping("/list")
-	public void list(Model model) {
-		List<BookDTO> list = dao.list();
-		model.addAttribute("list", list);
-	}
-
-
-}
+//package com.multi.spring.book.controller;
+//
+//import java.util.List;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//import com.multi.spring.book.model.dto.BookDTO;
+//
+//
+//@Controller
+//@RequestMapping("/book") 
+//public class BookController {
+//	
+//	@GetMapping("/book")
+//	public void foodForm() {
+//		
+//	}
+//	
+//	@GetMapping("/list")
+//	public void list(Model model) {
+//		List<BookDTO> list = dao.list();
+//		model.addAttribute("list", list);
+//	}
+//
+//
+//}
