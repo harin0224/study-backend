f4d0e09448d3832ccfc4f43a114bcb5457275ec6
HyesonAnn <side111229@gmail.com>
2024-06-13 14:53:13 +0900

    spring boot 2

--- 06_springboot/chap03_security/src/main/java/com/multi/authentication/model/service/AuthenticationService.java
+++
@@ -1,5 +0,0 @@
-package com.multi.authentication.model.service;
-
-public interface AuthenticationService {
-
-}
--- 06_springboot/chap03_security/src/main/java/com/multi/chap03security/config/SpringSecurityConfiguration.java
+++ 06_springboot/chap03_security/src/main/java/com/multi/chap03security/config/SpringSecurityConfiguration.java
@@ -1,12 +1,38 @@
 package com.multi.chap03security.config;
 
+import com.multi.chap03security.authentication.model.service.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import java.util.List;
+import java.util.Map;
+
+@Configuration
+@EnableWebSecurity
 public class SpringSecurityConfiguration {
+
+    private final AuthenticationService authenticationService;
+
+    @Autowired
+    public SpringSecurityConfiguration(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 
 
@@ -17,19 +43,46 @@ public class SpringSecurityConfiguration {
                 "/css/**", "/js/**", "/images/**"
         );
     }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)       // 이게 뭐지?   토근 위조 방지 안쓰겠다
+
+        Map<String, List<String>> permitListMap = authenticationService.getPermitListMap();
+        List<String> adminPermitList = permitListMap.get("adminPermitList");
+        List<String> memberPermitList = permitListMap.get("memberPermitList");
+
+        adminPermitList.forEach(url -> System.out.println("admin permit list : " + url));
+        memberPermitList.forEach(url -> System.out.println("member permit list : " + url));
+
+
+
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorizeHttpRequests)->authorizeHttpRequests
+//                         .requestMatchers("/menu/**").authenticated() // 권한확인
+//                        .requestMatchers("/admin/**").hasRole("ADMIN") // /admin/** 패턴에 일치하는 모든 경로에 대해 "ADMIN" 역할을 가진 사용자만 접근
+//                        .requestMatchers("/order/**").hasAnyRole("ADMIN", "MEMBER")
+                                .requestMatchers(memberPermitList.toArray(new String[memberPermitList.size()])).hasAnyRole("MEMBER", "ADMIN")
+                                .requestMatchers(adminPermitList.toArray(new String[adminPermitList.size()])).hasRole("ADMIN")
+
+                                .anyRequest().permitAll()
+                )
                 .formLogin(form -> form
-                        .loginPage("/member/login")
+                        .loginPage(("/member/login"))
                         .usernameParameter("username")
                         .passwordParameter("password")
-                        .defaultSuccessUrl("/",true)  // 성공했을 때 디폴트로 가는 url(루트 페이지로 가게)
-                        .failureForwardUrl("/error/login") // 실패했을 때
-                        );
-        return http.build();    // 위를 리턴해서 빌드하겠다
+                        .defaultSuccessUrl("/",true)    // 성공시 디폴트로 가는 url(루트 페이지로 가게)
+                        .failureForwardUrl("/error/login")  //실패했을 때
 
+                )
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+                        .deleteCookies("JSESSIONID")
+                        .invalidateHttpSession(true)
+                        .logoutSuccessUrl("/"))
+                .exceptionHandling((exception) -> exception.accessDeniedPage("/error/denied"));
+
+        return http.build();
 
     }
+
 }
